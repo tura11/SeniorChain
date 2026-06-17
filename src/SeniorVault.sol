ebe422fd84a238b8ea4309d0b9ef15b5b0bff91b
Tymek <tymek_uramek@wp.pl>
2026-06-17 12:52:27 +0200

    Deposit eth function

--- src/SeniorVault.sol
+++ src/SeniorVault.sol
@@ -5,4 +5,13 @@ pragma solidity ^0.8.31;
 
 contract SeniorVault {
 
+    mapping(address => uint256) private _balances; 
+
+
+    event DepositedEth(address indexed user, uint256 amount);
+
+    function deposit() external payable {
+        _balances[msg.sender] +=  msg.value;
+        emit DepositedEth(msg.sender, msg.value);
+    }
 }
