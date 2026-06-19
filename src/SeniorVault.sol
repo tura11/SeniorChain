2938aa1db3a3a6829e0004fbb6f20a014c751a42
Tymek <tymek_uramek@wp.pl>
2026-06-19 20:03:58 +0200

    DepositERC20

--- src/SeniorVault.sol
+++ src/SeniorVault.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.31;
 
-
+import {IERC20} from "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20.sol";
 
 contract SeniorVault {
 
     mapping(address => uint256) private _balances; 
 
+    IERC20 public token;
 
     event DepositedEth(address indexed user, uint256 amount);
 
@@ -16,5 +17,7 @@ contract SeniorVault {
     }
 
 
-    function depositERC20(uint256 amount) external{}
+    function depositERC20(uint256 amount) external{
+        token.safeTransferFrom(msg.sender, address(this), amount);
+    }
 }
