5bea700e135ae0ae11046c871ec6cd3d8ef1ab34
Tymek <tymek_uramek@wp.pl>
2026-06-21 14:56:03 +0200

    Fixing bugs

--- src/SeniorVault.sol
+++ src/SeniorVault.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.31;
 
 import {IERC20} from "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/SafeERC20.sol";
 
 contract SeniorVault {
+    using SafeERC20 for IERC20;
 
-    mapping(address => uint256) private _balances; 
+    
 
-    IERC20 public token;
 
     event DepositedEth(address indexed user, uint256 amount);
-    event DepositedERC20(address indexed user, uint256 amount);
+    event DepositedERC20(address indexed token, uint256 amount);
 
     struct Request{
         address from;
@@ -19,27 +20,26 @@ contract SeniorVault {
     }
 
 
+    address public constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+
     mapping(uint256 => Request) private _requests;
+    mapping(address => bool ) public isWhiteListed;
+    mapping(address => uint256) private _balances; 
 
 
     function deposit() external payable {
-        _balances[msg.sender] +=  msg.value;
+        _balances[ETH_ADDRESS] +=  msg.value;
         emit DepositedEth(msg.sender, msg.value);
     }
 
 
-    function depositERC20(uint256 amount) external{
-        token.safeTransferFrom(msg.sender, address(this), amount);
-
-        emit DepositedERC20(msg.sender, amount);
+    function depositERC20(address tokenAddress, uint256 amount) external {
+        require(isWhiteListed[tokenAddress], "Token not whitelisted");
+        IERC20(tokenAddress).safeTransferFrom(msg.sender, address(this), amount);
+        _balances[tokenAddress] += amount;
+        emit DepositedERC20(tokenAddress, amount);
     }
 
 
-    function _createDepositRequest(address from, uint256 amount, uint256 reqeuestId) internal {
-        _requests[reqeuestId] = Request({
-            from: from,
-            amount: amount,
-            approved: false
-        });
-    }
 }
