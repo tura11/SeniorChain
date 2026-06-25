629a3fde1112c46ab3743629bb1134d4b48e39c9
Tymek <tymek_uramek@wp.pl>
2026-06-25 20:23:31 +0200

    Preapre tests

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,3 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+
+import {SeniorVault} from "../src/SeniorVault.sol";
+import {Test} from "forge-std/test/Test.sol";
+
+
+contract SeniorVaultTest is Test{}
