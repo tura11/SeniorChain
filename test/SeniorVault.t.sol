d22d68f3f075794c8825f5cdb65f5a00d228cc86
Tymek <tymek_uramek@wp.pl>
2026-07-05 11:54:12 +0200

    100% coverage

--- test/SeniorVault.t.sol
+++ test/SeniorVault.t.sol
@@ -7,6 +7,8 @@ import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract SeniorVaultTest is Test {
     event DepositedEth(address indexed user, uint256 amount);
+    event DepositedToken(address indexed user, uint256 amount);
+    event WithdrawedERC20(address indexed token, uint256 amount);
 
     address public senior;
     address public guardian;
@@ -72,5 +74,235 @@ contract SeniorVaultTest is Test {
         vm.prank(senior);
         vm.expectRevert(SeniorVault.SeniorVault__AddressNotWhiteListed.selector);
         vault.depositERC20(address(token), 500e6);
+    }
+
+    function testDepositTokenEmitEvent() public {
+        vm.prank(senior);
+        vault.proposeToken(address(token));
+        vm.prank(guardian);
+        vault.approveToken(address(token));
+        vm.prank(senior);
+        emit DepositedToken(address(token), 500e6);
+        vault.depositERC20(address(token), 500e6);
+    }
+
+
+    function testProposeAndApproveGuardian() public {
+        address guardian2 = makeAddr("guardian2");
+        vm.prank(senior);
+        vault.proposeGuardian(guardian2);
+        vm.prank(guardian);
+        vault.approveNewGuardian();
+        assertEq(vault.guardian(), guardian2);
+    }
+
+    function testProposeSafeAddress() public {
+        address safeAddress1  = makeAddr("safeAddress1");
+        vm.prank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+        assertEq(vault.isAddressWhiteListed(safeAddress1), true);
+    }
+
+
+     /////////////////////////////////////////
+    /////////// WITHDRAW ETH TESTS //////////
+    /////////////////////////////////////////
+
+    function testWithdrawEth() public {
+        address safeAddress1 = makeAddr("safeAddress1");
+
+        vm.prank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+
+        vm.prank(senior);
+        vault.deposit{value: 5 ether}();
+
+        uint256 recipientBalanceBefore = safeAddress1.balance;
+
+        vm.prank(senior);
+        vault.withdrawETH(safeAddress1, 2 ether);
+
+        assertEq(vault.getUserTokenBalance(ETH_ADDRESS), 3 ether);
+        assertEq(safeAddress1.balance, recipientBalanceBefore + 2 ether);
+    }
+
+    function testWithdrawEthRevertIfNotWhiteListed() public {
+        address notWhiteListed = makeAddr("notWhiteListed");
+
+        vm.prank(senior);
+        vault.deposit{value: 5 ether}();
+
+        vm.prank(senior);
+        vm.expectRevert(SeniorVault.SeniorVault__AddressNotWhiteListed.selector);
+        vault.withdrawETH(notWhiteListed, 1 ether);
+    }
+
+    function testWithdrawEthRevertIfNotEnoughMoney() public {
+        address safeAddress1 = makeAddr("safeAddress1");
+
+        vm.prank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+
+        vm.prank(senior);
+        vault.deposit{value: 1 ether}();
+
+        vm.prank(senior);
+        vm.expectRevert(SeniorVault.SeniorVault__NotEnoughMoney.selector);
+        vault.withdrawETH(safeAddress1, 2 ether);
+    }
+
+    function testWithdrawEthRevertIfNotSenior() public {
+        address safeAddress1 = makeAddr("safeAddress1");
+
+        vm.prank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+
+        vm.prank(senior);
+        vault.deposit{value: 1 ether}();
+
+        vm.prank(guardian);
+        vm.expectRevert(SeniorVault.SeniorVault__NotSenior.selector);
+        vault.withdrawETH(safeAddress1, 1 ether);
+    }
+
+    function testWithdrawEthRevertIfTransferFailed() public {
+        RejectEther rejecter = new RejectEther();
+
+        vm.prank(senior);
+        vault.proposesSafeAddresses(address(rejecter));
+        vm.prank(guardian);
+        vault.approveSafeAddress(address(rejecter));
+
+        vm.prank(senior);
+        vault.deposit{value: 1 ether}();
+
+        vm.prank(senior);
+        vm.expectRevert(SeniorVault.SeniorVault__TransferFailed.selector);
+        vault.withdrawETH(address(rejecter), 1 ether);
+    }
+
+    /////////////////////////////////////////
+    /////////// WITHDRAW ERC20 TESTS ////////
+    /////////////////////////////////////////
+
+    function testWithdrawErc20() public {
+        address safeAddress1 = makeAddr("safeAddress1");
+
+        vm.startPrank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.stopPrank();
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+
+        vm.startPrank(senior);
+        vault.proposeToken(address(token));
+        vm.stopPrank();
+        vm.prank(guardian);
+        vault.approveToken(address(token));
+
+        vm.prank(senior);
+        vault.depositERC20(address(token), 500e6);
+
+        vm.prank(senior);
+        vault.withdrawERC20(safeAddress1, 200e6, address(token));
+
+        assertEq(vault.getUserTokenBalance(address(token)), 300e6);
+        assertEq(token.balanceOf(safeAddress1), 200e6);
+    }
+
+    function testWithdrawErc20RevertIfNotWhiteListed() public {
+        address notWhiteListed = makeAddr("notWhiteListed");
+
+        vm.prank(senior);
+        vault.proposeToken(address(token));
+        vm.prank(guardian);
+        vault.approveToken(address(token));
+
+        vm.prank(senior);
+        vault.depositERC20(address(token), 500e6);
+
+        vm.prank(senior);
+        vm.expectRevert(SeniorVault.SeniorVault__AddressNotWhiteListed.selector);
+        vault.withdrawERC20(notWhiteListed, 100e6, address(token));
+    }
+
+    function testWithdrawErc20RevertIfNotEnoughMoney() public {
+        address safeAddress1 = makeAddr("safeAddress1");
+
+        vm.prank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+
+        vm.prank(senior);
+        vault.proposeToken(address(token));
+        vm.prank(guardian);
+        vault.approveToken(address(token));
+
+        vm.prank(senior);
+        vault.depositERC20(address(token), 100e6);
+
+        vm.prank(senior);
+        vm.expectRevert(SeniorVault.SeniorVault__NotEnoughMoney.selector);
+        vault.withdrawERC20(safeAddress1, 200e6, address(token));
+    }
+
+    function testWithdrawErc20RevertIfNotSenior() public {
+        address safeAddress1 = makeAddr("safeAddress1");
+
+        vm.prank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+
+        vm.prank(senior);
+        vault.proposeToken(address(token));
+        vm.prank(guardian);
+        vault.approveToken(address(token));
+
+        vm.prank(senior);
+        vault.depositERC20(address(token), 100e6);
+
+        vm.prank(guardian);
+        vm.expectRevert(SeniorVault.SeniorVault__NotSenior.selector);
+        vault.withdrawERC20(safeAddress1, 50e6, address(token));
+    }
+
+    function testWithdrawErc20EmitEvent() public {
+        address safeAddress1 = makeAddr("safeAddress1");
+
+        vm.prank(senior);
+        vault.proposesSafeAddresses(safeAddress1);
+        vm.prank(guardian);
+        vault.approveSafeAddress(safeAddress1);
+
+        vm.prank(senior);
+        vault.proposeToken(address(token));
+        vm.prank(guardian);
+        vault.approveToken(address(token));
+
+        vm.prank(senior);
+        vault.depositERC20(address(token), 500e6);
+
+        vm.prank(senior);
+        vm.expectEmit(true, false, false, true);
+        emit WithdrawedERC20(address(token), 200e6);
+        vault.withdrawERC20(safeAddress1, 200e6, address(token));
+    }
+}
+
+
+
+contract RejectEther {
+    receive() external payable {
+        revert();
     }
 }
