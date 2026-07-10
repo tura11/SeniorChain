1f7df4cc2a96df70b586b172071417d252a06784
Tymek <tymek_uramek@wp.pl>
2026-07-10 19:11:10 +0200

    Pause implementation

--- src/SeniorVaultFactory.sol
+++ src/SeniorVaultFactory.sol
@@ -2,28 +2,35 @@
 pragma solidity 0.8.31;
 
 import {SeniorVault} from "./SeniorVault.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 
-contract SeniorVaultFactory {
+contract SeniorVaultFactory is Ownable, Pausable {
     error InvalidAddress();
     error VaultAlredyExist();
 
     event VaultCreated(address indexed user, address indexed vault);
     
 
-    address senior;
-
-
 
     mapping(address senior => address vault) public seniorToVault;
 
 
 
-    function createVault() external returns(address){
+    function createVault() external whenNotPaused returns(address){
         if(seniorToVault[msg.sender] != address(0)) revert VaultAlredyExist();
         SeniorVault vault = new SeniorVault(msg.sender);
         seniorToVault[msg.sender] = address(vault);
         emit VaultCreated(msg.sender, address(vault));
         return address(vault);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
 }
